789cd65097b660760aac873fdbe7f0d6313067f2
hasee <hasee@KQD278>
2016-05-06 10:45:08 +0800

    跟踪单报表工序名称模糊匹配

---
+++ ERPClient/ERP.Winform/Report/ProcessType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ERP.Winform.Report
+{
+    public enum ProcessType
+    {
+        粗拉,
+        中拉及清洗,
+        电镀,
+        细拉半成品1,
+        半成品退火,
+        细拉半成品2,
+        细拉成品,
+        成品清洗,
+        成品退火,
+        成品绕线,
+        成品内包
+    }
+
+}
