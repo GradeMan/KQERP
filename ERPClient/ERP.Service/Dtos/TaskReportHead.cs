614103c26f179855538f97f52e081229c9f536ad
hasee <hasee@KQD278>
2016-05-06 17:07:24 +0800

    跟踪单报表更改-->前制程

---
+++ ERPClient/ERP.Service/Dtos/TaskReportHead.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ERP.Service.Dtos
+{
+    public class TaskReportHead
+    {
+        public string TaskNo { get; set; }
+
+        public string JobLotNo { get; set; }
+
+        public string CustCode { get; set; }
+
+        public string SoNo { get; set; }
+
+        public decimal? SoQty { get; set; }
+
+        public string PartNo { get; set; }
+
+        public string PartSpec { get; set; }
+
+        public string ProcessFlow { get; set; }
+    }
+}
